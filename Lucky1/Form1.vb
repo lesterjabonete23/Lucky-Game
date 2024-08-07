b041fd2583f47afc10f824c76e754025ce92bfb1
Lester Jabonete <88809118+lesterjabonete23@users.noreply.github.com>
2024-08-07 10:26:50 +0800

    Update Form1.vb

--- Lucky1/Form1.vb
+++ Lucky1/Form1.vb
@@ -1,25 +1,4 @@
 ﻿Public Class Form1
-
-    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
-        End
-    End Sub
-
-    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Randomize()
-    End Sub
-
-    Private Sub Label3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
-
-    End Sub
-
-    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
-
-    End Sub
-
-    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label1.Click
-
-    End Sub
-
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         PictureBox1.Visible = False ' hide picture
         Label1.Text = CStr(Int(Rnd() * 10)) ' pick numbers
@@ -33,11 +12,4 @@
         End If
     End Sub
 
-    Private Sub Label4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label4.Click
-
-    End Sub
-
-    Private Sub PictureBox1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox1.Click
-
-    End Sub
 End Class
